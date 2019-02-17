9206aec767bd617e87d03cbc3b3c442e480a11ec
Sarahhorsewayho <171233887@qq.com>
2019-02-17 20:57:30 +0800

    modify reverse

--- Reverse Intenger.cpp
+++ Reverse Intenger.cpp
@@ -3,9 +3,9 @@ public:
     int reverse(int x) {
         long  res = 0;
         while(x) {
-            res = res*10 + x%10;
+            res = res * 10 + x % 10;
             x /= 10;
         }
-        return (res<INT_MIN || res>INT_MAX) ? 0 : res;
+        return (res < INT_MIN || res > INT_MAX) ? 0 : res;
     }
 };
